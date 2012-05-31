238dc652113b7524fab0c9e4af0296a8ddd1cb8b
Guillaume Miranda <guillaumemiranda@msn.com>
2012-05-31 10:42:22 +0200

    Test concluant de la création et de l'affichage du paquet de carte

--- jeu.py
+++ jeu.py
@@ -1,15 +1,19 @@
 import random
 from carte import Carte
-from main import main
+from main import Main
 
 class jeu:
-
-couleur=["H","S","D","C"]
-nom_carte=[2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+	
+	couleur=["H","S","D","C"]
+	nom_carte=[2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
 	def __init__(self):
-		paquet=Main(52)
-		for i in couleur:
-			for i in nom_carte:
-				carte=Carte(couleur,nom_carte)
+		self.paquet=Main(52)
+		for i in self.couleur:
+			for j in self.nom_carte:
+				carte=Carte(i,j)
 				self.paquet.ajouterCarte(carte)
+
+				
+	def affichePaquet(self):
+		self.paquet.afficher()
 
--- test_proj.py
+++ test_proj.py
@@ -1,8 +1,11 @@
-
 # -*- coding: utf-8 -*-
+
 from carte import Carte
 from joueurHumain import joueurHumain
+from jeu import jeu
 
+j=jeu()
+j.affichePaquet()
 joueur1=joueurHumain("Bob",27)
 carte1=Carte("H",1)
 joueur1.recevoirCarte(carte1)
