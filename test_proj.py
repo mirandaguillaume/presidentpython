2fc1d1af2f7f681f05eb4f459d2e784ae24d1e87
Guillaume Miranda <guillaumemiranda@msn.com>
2012-06-04 10:42:13 +0200

    Ajout du mélange du paquet de carte (non testé)

--- test_proj.py
+++ test_proj.py
@@ -2,11 +2,7 @@
 
 from carte import Carte
 from joueurHumain import joueurHumain
-from jeu import jeu
+from jeu import paquet
 
 j=jeu()
 j.affichePaquet()
-joueur1=joueurHumain("Bob",27)
-carte1=Carte("H",1)
-joueur1.recevoirCarte(carte1)
-joueur1.afficher()
